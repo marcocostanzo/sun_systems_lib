31c84a73698cf188407574d63b6ed48c86918563
Marco Costanzo <marco.costanzo@unicampania.it>
2018-10-08 16:42:22 +0200

    changed folder organization in unicampania_systems_lib (added header files)

---
+++ include/TF_SISO/TF_INTEGRATOR.h
@@ -0,0 +1,69 @@
+
+/*
+    TF_INTEGTATOR Class
+
+    Copyright 2018 Università della Campania Luigi Vanvitelli
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef TF_INTEGTATOR_LIB
+#define TF_INTEGTATOR_LIB
+
+#include "TF_SISO/TF_SISO.h"
+
+class TF_INTEGTATOR : public TF_SISO{
+private:
+
+	TF_INTEGTATOR(); //NO DEFAULT CONSTRUCTOR
+
+protected:
+
+    double _gain;
+
+public:
+/*===============CONSTRUCTORS===================*/
+    TF_INTEGRATOR(double Ts, double gain);
+    TF_INTEGRATOR(double Ts);
+/*==============================================*/
+
+/*===============DESTRUCTOR===================*/	
+//	~TF_INTEGRATOR(); //(destructor)
+/*==============================================*/
+
+/*=============GETTER===========================*/
+/*==============================================*/
+
+/*=============SETTER===========================*/
+    void setState(double output);
+/*==============================================*/
+
+/*=============SETTER FROM FILE===========================*/
+/*========================================================*/
+
+/*=============RUNNER===========================*/
+    virtual double apply( double uk);
+/*==============================================*/
+
+/*=============VARIE===========================*/
+/*==============================================*/
+
+};
+
+
+/*=============STATIC FUNS===========================*/
+
+/*==============================================*/
+
+#endif
