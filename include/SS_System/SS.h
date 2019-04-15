edb8b3e211c1f8be88505de059d7382051697c0b
Marco Costanzo <marco.costanzo@unicampania.it>
2019-04-15 16:38:21 +0200

    generic SS class

---
+++ include/SS_System/SS.h
@@ -0,0 +1,68 @@
+/*
+    SS Class (State Space System)
+
+    Copyright 2019 Università della Campania Luigi Vanvitelli
+
+    Author: Marco Costanzo <marco.costanzo@unicampania.it>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef SS_H
+#define SS_H
+
+#include "boost/function.hpp"
+#include "SS_System/SS_Interface.h"
+
+typedef boost::function<TooN::Vector<>(const TooN::Vector<>&, const TooN::Vector<>&)> SS_FCN;
+
+class SS : public SS_Interface {
+
+private:
+
+SS(); //avoid default constructor
+
+protected:
+
+//State
+SS_FCN _state_fcn;
+SS_FCN _output_fcn;
+
+public:
+    
+virtual ~SS() = default;
+
+SS(const SS_FCN& state_fcn, const SS_FCN& output_fcn, TooN::Vector<>& initial_state, double Ts);
+
+SS(const SS_FCN& state_fcn, const SS_FCN& output_fcn, int order, double Ts);
+
+/*
+    This function does not change/use the internal state of the object
+    It evaluates the next state given the previous state and the current input
+*/
+virtual TooN::Vector<> state_f( const TooN::Vector<>& state, const TooN::Vector<>& input ) const override;
+
+/*
+    This function does not change/use the internal state of the object
+    It evaluates the next output given the previous state and the current input
+*/
+virtual TooN::Vector<> output_f( const TooN::Vector<>& state, const TooN::Vector<>& input ) const override;
+
+virtual void display() override;
+
+};
+
+using SS_Ptr = std::unique_ptr<SS>;
+
+#endif
---
+++ src/SS_System/SS.cpp
@@ -0,0 +1,60 @@
+/*
+    SS Class (State Space System)
+
+    Copyright 2019 Università della Campania Luigi Vanvitelli
+
+    Author: Marco Costanzo <marco.costanzo@unicampania.it>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "SS_System/SS.h"
+
+using namespace TooN;
+
+SS::SS(const SS_FCN& state_fcn, const SS_FCN& output_fcn, Vector<>& initial_state, double Ts) 
+    : SS_Interface(initial_state, Ts),
+      _state_fcn(state_fcn),
+      _output_fcn(output_fcn)
+      {}
+
+SS::SS(const SS_FCN& state_fcn, const SS_FCN& output_fcn, int order, double Ts) 
+    : SS_Interface(order, Ts),
+      _state_fcn(state_fcn),
+      _output_fcn(output_fcn)
+      {}
+
+/*
+    This function does not change/use the internal state of the object
+    It evaluates the next state given the previous state and the current input
+*/
+Vector<> SS::state_f( const Vector<>& state, const Vector<>& input ) const
+{
+    return _state_fcn( state, input );
+}
+
+/*
+    This function does not change/use the internal state of the object
+    It evaluates the next output given the previous state and the current input
+*/
+Vector<> SS::output_f( const Vector<>& state, const Vector<>& input ) const
+{
+    return _output_fcn( state, input );
+}
+
+void SS::display()
+{
+    std::cout << "[SS]" << std::endl <<
+                 "state = " << _state << std::endl;
+}
