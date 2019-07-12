6edf93c3775848ded6389c200d8a297693e2b926
Marco Costanzo <marco.costanzo@unicampania.it>
2019-07-12 12:30:11 +0200

    Continuous

--- include/sun_systems_lib/Continuous/Continuous_System.cpp
+++ include/sun_systems_lib/Continuous/Continuous_System.cpp
@@ -1,5 +1,5 @@
 /*
-    Generic SS from external functions
+    State Space Continuous Time System Class
 
     Copyright 2019 Università della Campania Luigi Vanvitelli
 
@@ -19,10 +19,10 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef SS_H
-#define SS_H
+#ifndef CONTINUOUS_SYSTEM_H
+#define CONTINUOUS_SYSTEM_H
 
-#include <sun_systems_lib/SS/SS_Interface.cpp>
+#include <sun_systems_lib/Continuous/Continuous_System_Interface.cpp>
 #include "boost/function.hpp"
 
 #ifndef SS_FUNCTION_TYPES
@@ -31,7 +31,7 @@ typedef boost::function<TooN::Vector<>(const TooN::Vector<>&, const TooN::Vector
 typedef boost::function<TooN::Matrix<>(const TooN::Vector<>&, const TooN::Vector<>&)> SS_JACOB_FCN;
 #endif
 
-class SS : public SS_Interface
+class Continuous_System : public Continuous_System_Interface
 {
 
 private:
@@ -40,34 +40,55 @@ protected:
 
 SS_FCN state_fcn_, output_fcn_;
 SS_JACOB_FCN jacob_state_fcn_, jacob_output_fcn_;
-unsigned int dim_output_, dim_input_;
+unsigned int dim_state_, dim_output_, dim_input_;
 
 public:
 
-SS( unsigned int dim_state, 
+Continuous_System( 
+    unsigned int dim_state, 
     unsigned int dim_output, 
     unsigned int dim_input, 
     const SS_FCN& state_fcn, 
     const SS_FCN& output_fcn, 
     const SS_JACOB_FCN& jacob_state_fcn = NULL, 
     const SS_JACOB_FCN& jacob_output_fcn = NULL )
-    :SS_Interface( TooN::Zeros(dim_state), TooN::Zeros(dim_output) ),
+    :
+    dim_state_(dim_state),
+    dim_output_(dim_output),
+    dim_input_(dim_input),
     state_fcn_(state_fcn),
     output_fcn_(output_fcn),
     jacob_state_fcn_(jacob_state_fcn),
-    jacob_output_fcn_(jacob_output_fcn),
-    dim_output_(dim_output),
-    dim_input_(dim_input)
+    jacob_output_fcn_(jacob_output_fcn)
     {}
 
-SS(const SS& ss) = default;
+/*
+Continuous_System(  
+    const SS_FCN& state_fcn, 
+    const SS_FCN& output_fcn, 
+    const SS_JACOB_FCN& jacob_state_fcn = NULL, 
+    const SS_JACOB_FCN& jacob_output_fcn = NULL 
+    )
+    :Continuous_System( 
+        0, 
+        0, 
+        0, 
+        state_fcn, 
+        output_fcn, 
+        jacob_state_fcn, 
+        jacob_output_fcn 
+        )
+    {}
+    */
 
-virtual SS* clone() const override
+Continuous_System(const Continuous_System& ss) = default;
+
+virtual Continuous_System* clone() const override
 {
-    return new SS(*this);
+    return new Continuous_System(*this);
 }
 
-virtual ~SS() override = default;
+virtual ~Continuous_System() override = default;
 
 inline virtual const TooN::Vector<> state_fcn( const TooN::Vector<>& x_k_1, const TooN::Vector<>& u_k ) const override
 {
@@ -89,13 +110,6 @@ inline virtual const TooN::Matrix<> jacob_output_fcn( const TooN::Vector<>& x_k,
     return jacob_output_fcn_(x_k, u_k);
 }
 
-inline virtual const TooN::Vector<>& apply( const TooN::Vector<>& input ) override
-{
-    state_ = state_fcn_( state_, input );
-    output_ = output_fcn_( state_, input );
-    return output_;
-}
-
 virtual const unsigned int getSizeInput() const override
 {
     return dim_input_;
@@ -106,17 +120,18 @@ virtual const unsigned int getSizeOutput() const override
     return dim_output_;
 }
 
-virtual void display() const override
+virtual const unsigned int getSizeState() const override
 {
-    std::cout <<
-    "SS:" << std::endl <<
-    "Functions are defined run-time" << std::endl <<
-    "state: " << state_ << std::endl <<
-    "SS [END]" << std::endl;
+    return dim_state_;
+}
+
+virtual void display() const
+{
+    std::cout << BOLDYELLOW "WARNING! display() not implemented for Continuous_System_Interface" CRESET << std::endl;
 }
 
 };
 
-using SS_Ptr = std::unique_ptr<SS>;
+using Continuous_System_Ptr = std::unique_ptr<Continuous_System>;
 
 #endif
